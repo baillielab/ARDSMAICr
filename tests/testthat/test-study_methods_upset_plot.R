f88e30fbbac7d0c1a8c3dddaeb10ca5b201a7275
Johnny Millar <jonathan.millar@ed.ac.uk>
2023-06-20 17:08:35 +0100

    fix tests

--- tests/testthat/test-study_methods_upset_plot.R
+++
@@ -1,9 +0,0 @@
-## methods_upset()
-
-test_that("creates upset plot as expected", {
-  sample_genes <- readRDS(test_path("testdata", "sample_genes.rds"))
-  vdiffr::expect_doppelganger(
-    title = "create upset",
-    fig = suppressWarnings(methods_upset(sample_genes)),
-  )
-})
